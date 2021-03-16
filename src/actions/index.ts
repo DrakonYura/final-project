74d51df2bd404411da3e3ec997b999d7dea26a31
Yura Drakovanov <drakonyurok@yandex.ru>
2021-03-16 23:27:41 +0300

    Remove some functions

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -51,48 +51,6 @@ const addToMyPokemons = ( caughtTime:string, pokemon: PokemonItemType ) : AddToM
     }
 }
 
-const fetchPokemonsData = ():ThunkAction<void, AppStateType, unknown, ActionsType> => {
-    return (dispatch) => {
-        axios.get(`http://localhost:4000/pokemons`)
-            .then(res => { dispatch(pokemonListLoaded( res.data )) })
-            .catch(() => { dispatch(pokemonListError()) });
-    }
-}
-
-const fetchMyPokemons = () :ThunkAction<void, AppStateType, unknown, ActionsType> => {
-    return (dispatch) => {
-        axios.get(`http://localhost:4000/mypokemons`)
-        .then((res)=> {dispatch(myPokemonListLoaded(res.data))})
-        .catch(() => { dispatch(pokemonListError()) });
-    }
-}
-
-
-const fetchPageData = ( pageNumber: number ): ThunkAction<void, AppStateType, unknown, ActionsType> => {
-    return ( dispatch) => {
-        axios.get(`http://localhost:4000/pokemons`, {
-            params: {
-                _page: pageNumber,
-                _limit: 30
-            }
-        })
-            .then(res => { dispatch(pokemonListLoaded( res.data )) })
-            .catch(() => { dispatch(pokemonListError()) });
-    }
-}
-
-const postMyPokemons = (caughtTime:string, pokemon: PokemonItemType) : ThunkAction<void, AppStateType, unknown, ActionsType> => {
-    return (dispatch) => {
-        axios.post(`http://localhost:4000/mypokemons`, {
-            name: pokemon.name,
-            id: pokemon.id,
-            time: caughtTime
-          })
-          .then(()=> {dispatch(addToMyPokemons(caughtTime, pokemon)) })
-          .catch(()=> {dispatch(pokemonListError())} );
-    }
-}
-
 export {
     pokemonListLoaded,
     myPokemonListLoaded,
